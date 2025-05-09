e9144020c583635d0acd2aad044adab3d76cc481
jcovar9 <jcovar9@uic.edu>
2025-05-09 18:38:24 -0500

    using pathbase /imageserver

--- Program.cs
+++ Program.cs
@@ -30,13 +30,21 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
-app.UseStaticFiles();
+app.UseStaticFiles(new StaticFileOptions
+{
+    RequestPath = "/imageserver"
+});
+app.UsePathBase("/imageserver");
 app.UseRouting();
 app.UseSession();
 app.UseAuthorization();
 app.MapControllers();
-app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=EnterDirectory}/{id?}");
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=EnterDirectory}/{id?}");
+});
 
 app.Run();
